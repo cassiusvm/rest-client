b4c7ee1bbce32dae0167ae60ad83bc58827241e6
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2023-09-11 16:36:56 -0300

    Rollback @Profile doesn't work with Interface

--- src/main/java/dev/danvega/restclient/config/HTTPInterfaceClient.java
+++ src/main/java/dev/danvega/restclient/config/HTTPInterfaceClient.java
@@ -3,13 +3,11 @@ package dev.danvega.restclient.config;
 import dev.danvega.restclient.service.JsonPlaceholderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
-@Profile("httpinterfaceclient")
 public class HTTPInterfaceClient {
 
     @Bean
